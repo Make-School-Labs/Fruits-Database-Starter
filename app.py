a84aba4fe5c4c2b5064660d29dc213f1a500799a
meredithcat <meredithcat@gmail.com>
2020-11-30 14:40:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
-from pymongo import MongoClient
-# from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 ####################################################
 # SETUP
